814a3e2876416e9874cf03078edbf9a6bd9e5034
smilee1111 <muskankc2005@gmail.com>
2025-02-27 23:05:19 +0545

    dashboard and task design initial

---
+++ Prvoir_Planner/app/src/main/java/com/example/pr_voir_planner/model/TaskModel.kt
@@ -0,0 +1,9 @@
+package com.example.pr_voir_planner.model
+
+data class Task(
+    val title: String,
+    val description: String,
+    val priority: String,
+    val dueTime: String,
+    val status: String
+)
